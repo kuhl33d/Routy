a60ac5b4597bcaa758dafe1f85d0b3fc6f13c37c
Khaled Abdel-Nasser <44317515+kuhl33d@users.noreply.github.com>
2025-02-19 01:55:06 +0000

    added server file

---
+++ Routy/backend/index.js
@@ -0,0 +1,9 @@
+// const express = require("express");
+
+import express from 'express';
+
+const app = express();
+
+app.listen(5000,()=>{
+    console.log("Server Started at http://localhost:5000");
+});
