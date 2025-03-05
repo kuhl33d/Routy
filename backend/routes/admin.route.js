d19738c42032832e86a75fa9666072a48d011b38
Khaled Abdel-Nasser <44317515+kuhl33d@users.noreply.github.com>
2025-03-05 15:15:49 +0000

    admin update

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -18,8 +18,8 @@ router.use(protectRoute);
 router.use(adminRoute);
 
 router.get("/dashboard", getDashboardStats);
-router.get("/overview", getSystemOverview); //edit to follow requirements
-router.get("/analytics", getAnalytics); // error
+router.get("/overview", getSystemOverview);
+router.get("/analytics", getAnalytics);
 
 // Not Tested
 router.get("/fullReportSys", getFullReview);
@@ -28,10 +28,10 @@ router.get("/schoolSubscriptions/:schoolId", schoolSubscriptions);
 // add subscription to the school per bus
 router.post("/subscription/:schoolId", addSubscription);
 // approve school to go in system
-router.put("/approveSchool/:schoolId", approveSchool); // error
+router.put("/approveSchool/:schoolId", approveSchool);
 // approve driver to go in system by school
-router.put("/approveDriver/:driverId", approveDriver); // not implemented
+router.put("/approveDriver/:driverId", approveDriver);
 // pay the bus fees (student) by school
-router.put("/payFees/:studentId", payFees); // not tested
+router.put("/payFees/:studentId", payFees);
 
 export default router;
